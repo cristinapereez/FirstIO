9beb49d6b85d23eb181054205df8ebc3573a1d92
cristinapereez <cperez58@mail.csuchico.edu>
2018-01-30 11:24:09 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// Author: Cristina Perez
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Cristina. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
